2d590fe5e0e81f42ea00722e88eea9d6adf8bcb9
jmadupalli <jayanth.madupalli@gmail.com>
2023-12-21 14:29:50 -0600

    Initial Commit

---
+++ blog-backend/src/main/java/com/redy/blogbackend/BlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.redy.blogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogBackendApplication.class, args);
+	}
+
+}
---
+++ blog-backend/src/test/java/com/redy/blogbackend/BlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redy.blogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
